aa9c08aecf79c52674d989e78bf166e0227375f7
anant114 <61869853+anant114@users.noreply.github.com>
2024-06-08 22:59:24 +0530

    Add files via upload

---
+++ assesment/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment: any = {
+    production: false,
+    weatherApiBaseUrl: 'http://api.weatherbit.io/v2.0/',
+
+    XRapidAPIKeyHeader: 'X-RapidAPI-Key',
+    XRapidAPIKeyValue: '5808d86449044958abe170532240806',
+
+    XRapidAPIHostHeader: 'host',
+    XRapidAPIHostValue: 'weatherapi.com',
+};
